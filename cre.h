e77ad173c103929ed5b07380c97e674ca78beafc
mu001999 <mu001999@outlook.com>
2018-06-06 21:37:45 +0800

    init

---
+++ cre.h
@@ -0,0 +1,34 @@
+#ifndef _CRE_H_
+#define _CRE_H_
+
+namespace cre
+{
+	class Pattern
+	{
+	public:
+
+		Pattern(char *pattern, int flags) {}
+		~Pattern() {}
+
+		int match(char *str)
+		{
+
+		}
+	};
+
+	class Cre
+	{
+	public:
+
+		Cre() {}
+		~Cre() {}
+
+		static int match(char *pattern, char *str, int flags)
+		{
+			Pattern _pattern(pattern, flags);
+			return _pattern.match(str);
+		}
+	};
+}
+
+#endif // _CRE_H_
